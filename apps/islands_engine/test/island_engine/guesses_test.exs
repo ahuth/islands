da92a0eee60d25b8b29c9b57e3a7ae04fd7a5417
Andrew Huth <andrew@huth.me>
2018-03-22 21:02:00 -0700

    Add function to make guesses

--- apps/islands_engine/test/island_engine/guesses_test.exs
+++ apps/islands_engine/test/island_engine/guesses_test.exs
@@ -1,8 +1,21 @@
 defmodule IslandsEngine.GuessesTest do
   use ExUnit.Case, async: true
+  alias IslandsEngine.Coordinate
   alias IslandsEngine.Guesses, as: Subject
 
   test "creating" do
     assert Subject.new == %Subject{hits: MapSet.new, misses: MapSet.new}
+  end
+
+  test "adding a hit" do
+    {:ok, coordinate} = Coordinate.new(8, 3)
+    guesses = Subject.new |> Subject.add(:hit, coordinate)
+    assert guesses.hits == MapSet.new([coordinate])
+  end
+
+  test "adding a miss" do
+    {:ok, coordinate} = Coordinate.new(9, 7)
+    guesses = Subject.new |> Subject.add(:miss, coordinate)
+    assert guesses.misses == MapSet.new([coordinate])
   end
 end
