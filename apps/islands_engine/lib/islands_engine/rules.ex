7ca0e4750c03f3372b619afa209f33b9fe21b902
Andrew Huth <andrew@huth.me>
2018-04-08 17:25:42 -0700

    Fix typo / bug

--- apps/islands_engine/lib/islands_engine/rules.ex
+++ apps/islands_engine/lib/islands_engine/rules.ex
@@ -2,8 +2,8 @@ defmodule IslandsEngine.Rules do
   alias __MODULE__
 
   defstruct state: :initialized,
-            player1: :island_not_set,
-            player2: :island_not_set
+            player1: :islands_not_set,
+            player2: :islands_not_set
 
   def new, do: %Rules{}
 
