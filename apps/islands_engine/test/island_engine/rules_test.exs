e75be19f41c5a08f3ba276d61ee6b81295a54959
Andrew Huth <andrew@huth.me>
2018-04-08 17:25:54 -0700

    Collapse state transition tests into a single test

--- apps/islands_engine/test/island_engine/rules_test.exs
+++ apps/islands_engine/test/island_engine/rules_test.exs
@@ -17,71 +17,49 @@ defmodule IslandsEngine.RulesTest do
       assert Subject.check(rules, :foobar) == :error
     end
 
-    test "from initialized with add_player" do
+    test "progressing through each state" do
+      # Initial state
       rules = Subject.new
+      assert rules == %Subject{state: :initialized, player1: :islands_not_set, player2: :islands_not_set}
+      # Start the game
       {:ok, rules} = Subject.check(rules, :add_player)
-      assert rules == %Subject{state: :players_set}
-    end
-
-    test "from players_set with position_islands when the player is set" do
-      rules = %Subject{state: :players_set, player1: :islands_set}
-      assert Subject.check(rules, {:position_islands, :player1}) == :error
-    end
-
-    test "from players_set with position_islands when the player is not set" do
-      rules = %Subject{state: :players_set, player1: :islands_not_set}
-      {:ok, new_rules} = Subject.check(rules, {:position_islands, :player1})
-      assert rules == new_rules
-    end
-
-    test "from players_set with set_islands when neither player is set" do
-      rules = %Subject{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}
+      assert rules == %Subject{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}
+      # Position an island
+      {:ok, rules} = Subject.check(rules, {:position_islands, :player1})
+      assert rules == %Subject{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}
+      # Position an island
+      {:ok, rules} = Subject.check(rules, {:position_islands, :player2})
+      assert rules == %Subject{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}
+      # Player one sets her islands
       {:ok, rules} = Subject.check(rules, {:set_islands, :player1})
-      assert rules.state == :players_set
-      assert rules.player1 == :islands_set
-    end
-
-    test "from players_set with set_islands when both players end up set" do
-      rules = %Subject{state: :players_set, player1: :islands_set, player2: :islands_not_set}
+      assert rules == %Subject{state: :players_set, player1: :islands_set, player2: :islands_not_set}
+      # Player one can no longer position islands
+      result = Subject.check(rules, {:position_islands, :player1})
+      assert result == :error
+      # Player two can still position islands
+      {:ok, rules} = Subject.check(rules, {:position_islands, :player2})
+      assert rules == %Subject{state: :players_set, player1: :islands_set, player2: :islands_not_set}
+      # Player two sets her islands and the next state is reached
       {:ok, rules} = Subject.check(rules, {:set_islands, :player2})
-      assert rules.state == :player1_turn
-      assert rules.player2 == :islands_set
-    end
-
-    test "from player1_turn with guess_coordinate for player 1" do
-      rules = %Subject{state: :player1_turn}
+      assert rules == %Subject{state: :player1_turn, player1: :islands_set, player2: :islands_set}
+      # Player two tries to guess, but it's not her turn
+      result = Subject.check(rules, {:guess_coordinate, :player2})
+      assert result == :error
+      # Player one guesses
       {:ok, rules} = Subject.check(rules, {:guess_coordinate, :player1})
-      assert rules.state == :player2_turn
-    end
-
-    test "from player1_turn with win_check when it is a win" do
-      rules = %Subject{state: :player1_turn}
-      {:ok, rules} = Subject.check(rules, {:win_check, :win})
-      assert rules.state == :game_over
-    end
-
-    test "from player1_turn with win_check when it is not a win" do
-      rules = %Subject{state: :player1_turn}
-      {:ok, rules} = Subject.check(rules, {:win_check, :no_win})
-      assert rules.state == :player1_turn
-    end
-
-    test "from player2_turn with guess_coordinate for player 2" do
-      rules = %Subject{state: :player2_turn}
+      assert rules == %Subject{state: :player2_turn, player1: :islands_set, player2: :islands_set}
+      # Player two guesses
       {:ok, rules} = Subject.check(rules, {:guess_coordinate, :player2})
-      assert rules.state == :player1_turn
-    end
-
-    test "from player2_turn with win_check when it is a win" do
-      rules = %Subject{state: :player2_turn}
-      {:ok, rules} = Subject.check(rules, {:win_check, :win})
-      assert rules.state == :game_over
-    end
-
-    test "from player2_turn with win_check when it is not a win" do
-      rules = %Subject{state: :player2_turn}
+      assert rules == %Subject{state: :player1_turn, player1: :islands_set, player2: :islands_set}
+      # Player one guesses
+      {:ok, rules} = Subject.check(rules, {:guess_coordinate, :player1})
+      assert rules == %Subject{state: :player2_turn, player1: :islands_set, player2: :islands_set}
+      # Check for a win when it's not yet over
       {:ok, rules} = Subject.check(rules, {:win_check, :no_win})
-      assert rules.state == :player2_turn
+      assert rules == %Subject{state: :player2_turn, player1: :islands_set, player2: :islands_set}
+      # Check for a win when it is over
+      {:ok, rules} = Subject.check(rules, {:win_check, :win})
+      assert rules == %Subject{state: :game_over, player1: :islands_set, player2: :islands_set}
     end
   end
 end
