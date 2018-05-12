58d06c4e4fbf04eb6b028861d164121fe8483891
Andrew Huth <andrew@huth.me>
2018-05-11 21:54:31 -0700

    Test positioning through our gen server

--- apps/islands_engine/test/island_engine/game_test.exs
+++ apps/islands_engine/test/island_engine/game_test.exs
@@ -17,4 +17,14 @@ defmodule IslandsEngine.GameTest do
     state = :sys.get_state(game)
     assert state.player2.name == "Fred"
   end
+
+  test "positioning", %{game: game} do
+    :ok = Subject.add_player(game, "Fred")
+    state = :sys.get_state(game)
+    refute Map.has_key?(state.player1.board, :square)
+
+    :ok = Subject.position_island(game, :player1, :square, 1, 1)
+    state = :sys.get_state(game)
+    assert Map.has_key?(state.player1.board, :square)
+  end
 end
