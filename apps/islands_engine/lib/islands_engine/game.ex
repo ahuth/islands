6570ac7d4bf87cd95754b8878b0547ae23c9810f
Andrew Huth <andrew@huth.me>
2018-05-12 07:53:17 -0700

    Add a (long) timeout to our GenServer

--- apps/islands_engine/lib/islands_engine/game.ex
+++ apps/islands_engine/lib/islands_engine/game.ex
@@ -3,6 +3,7 @@ defmodule IslandsEngine.Game do
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
 
   @players [:player1, :player2]
+  @timeout 60 * 60 * 24 * 1000
 
   def start_link(name) when is_binary(name) do
     GenServer.start_link(__MODULE__, name, name: via_tuple(name))
@@ -13,7 +14,7 @@ defmodule IslandsEngine.Game do
   def init(name) do
     player1 = %{name: name, board: Board.new, guesses: Guesses.new}
     player2 = %{name: nil, board: Board.new, guesses: Guesses.new}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
   end
 
   def add_player(game, name) when is_binary(name) do
@@ -40,7 +41,7 @@ defmodule IslandsEngine.Game do
       |> update_rules(rules)
       |> reply_success(:ok)
     else
-      :error -> {:reply, :error, state_data}
+      :error -> reply_error(state_data, :error)
     end
   end
 
@@ -55,10 +56,10 @@ defmodule IslandsEngine.Game do
       |> update_rules(rules)
       |> reply_success(:ok)
     else
-      :error -> {:reply, :error, state_data}
-      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state_data}
-      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, state_data}
-      {:error, :overlapping_island} -> {:reply, {:error, :overlapping_island}, state_data}
+      :error -> reply_error(state_data, :error)
+      {:error, :invalid_coordinate} -> reply_error(state_data, {:error, :invalid_coordinate})
+      {:error, :invalid_island_type} -> reply_error(state_data, {:error, :invalid_island_type})
+      {:error, :overlapping_island} -> reply_error(state_data, {:error, :overlapping_island})
     end
   end
 
@@ -71,35 +72,36 @@ defmodule IslandsEngine.Game do
       |> update_rules(rules)
       |> reply_success({:ok, board})
     else
-      :error -> {:reply, :error, state_data}
-      false -> {:reply, {:error, :not_all_islands_positioned}, state_data}
+      :error -> reply_error(state_data, :error)
+      false -> reply_error(state_data, {:error, :not_all_islands_positioned})
     end
   end
 
-  def handle_call({:guess_coordinate, player_key, row, col}, _from, state) do
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data) do
     opponent_key = opponent(player_key)
-    opponent_board = player_board(state, opponent_key)
+    opponent_board = player_board(state_data, opponent_key)
 
-    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player_key}),
+    with {:ok, rules} <- Rules.check(state_data.rules, {:guess_coordinate, player_key}),
          {:ok, coordinate} <- Coordinate.new(row, col),
          {hit_or_miss, forested_island, win_status, opponent_board} <- Board.guess(opponent_board, coordinate),
          {:ok, rules} <- Rules.check(rules, {:win_check, win_status})
     do
-      state
+      state_data
       |> update_board(opponent_key, opponent_board)
       |> update_guesses(player_key, hit_or_miss, coordinate)
       |> update_rules(rules)
       |> reply_success({hit_or_miss, forested_island, win_status})
     else
-      :error -> {:reply, :error, state}
-      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+      :error -> reply_error(state_data, :error)
+      {:error, :invalid_coordinate} -> reply_error(state_data, {:error, :invalid_coordinate})
     end
   end
 
   defp opponent(:player1), do: :player2
   defp opponent(:player2), do: :player1
   defp player_board(state_data, player), do: Map.get(state_data, player).board
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+  defp reply_error(state_data, reply), do: {:reply, reply, state_data, @timeout}
+  defp reply_success(state_data, reply), do: {:reply, reply, state_data, @timeout}
   defp update_board(state_data, player, board), do: Map.update!(state_data, player, fn player -> %{player | board: board} end)
   defp update_player2_name(state_data, name), do: put_in(state_data.player2.name, name)
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
